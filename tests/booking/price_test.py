86602d3841ffa08d93f32bf7c4327a3ddc645942
st4lk <myhappydo@gmail.com>
2014-04-21 23:44:30 +0400

    simplify days in test

--- tests/booking/price_test.py
+++ tests/booking/price_test.py
@@ -1,10 +1,14 @@
 from decimal import Decimal as D
-from django.utils.timezone import now
 from datetime import timedelta
-from nnmware.tests.base import BaseTestCase
-from model_mommy import mommy
-from nnmware.apps.booking.models import PlacePrice
+from django.utils.timezone import now
 from django import forms
+from model_mommy import mommy
+
+from nnmware.apps.booking.models import (PlacePrice, DISCOUNT_UNKNOWN,
+    DISCOUNT_NOREFUND, DISCOUNT_EARLY, DISCOUNT_LATER, DISCOUNT_PERIOD,
+    DISCOUNT_PACKAGE, DISCOUNT_HOLIDAY, DISCOUNT_SPECIAL, DISCOUNT_LAST_MINUTE,
+    DISCOUNT_CREDITCARD, DISCOUNT_NORMAL)
+from nnmware.tests.base import BaseTestCase
 
 
 class PlacePriceForm(forms.ModelForm):
@@ -12,13 +16,15 @@ class PlacePriceForm(forms.ModelForm):
         model = PlacePrice
 
 
-class PriceTestCase(BaseTestCase):
+class BasePriceTestCase(BaseTestCase):
     def setUp(self):
-        self.today = now()
+        super(BasePriceTestCase, self).setUp()
+        self.day1 = now()
         one_day = timedelta(days=1)
-        self.one_day = one_day
-        self.next_day = self.today + one_day
-        self.room = mommy.make('booking.Room')
+        for i in xrange(1, 7):
+            setattr(self, 'day{0}'.format(i+1), self.day1 + one_day*i)
+        self.hotel = mommy.make('booking.Hotel')
+        self.room = mommy.make('booking.Room', hotel=self.hotel)
         self.stl1 = mommy.make('booking.SettlementVariant',
             room=self.room, enabled=True, settlement=1)
         self.stl2 = mommy.make('booking.SettlementVariant',
@@ -26,32 +32,35 @@ class PriceTestCase(BaseTestCase):
         self.stl3 = mommy.make('booking.SettlementVariant',
             room=self.room, enabled=False, settlement=3)
         day_price = {
-            self.today: [(self.stl1, 10), (self.stl2, 15), (self.stl3, 20)],
-            self.next_day: [(self.stl1, 11), (self.stl2, 12), (self.stl3, 13)],
-            self.next_day+one_day: [(self.stl1, 14), (self.stl2, 15), (self.stl3, 16)],
+            self.day1: [(self.stl1, 10), (self.stl2, 15), (self.stl3, 20)],
+            self.day2: [(self.stl1, 11), (self.stl2, 12), (self.stl3, 13)],
+            self.day3: [(self.stl1, 14), (self.stl2, 15), (self.stl3, 16)],
         }
         for date, prices in day_price.items():
             for stl, price in prices:
                 mommy.make('booking.PlacePrice', date=date,
                     amount=price, settlement=stl)
 
+
+class PriceTestCase(BasePriceTestCase):
+
     def test_price_days(self):
-        td, nd, od = self.today, self.next_day, self.one_day
+        d1, d2, d3, d4 = self.day1, self.day2, self.day3, self.day4
         prices = [
             # one day
-            {'day_in': td, 'day_out': nd, "guests": 1, "total": D('10')},
-            {'day_in': td, 'day_out': nd, "guests": 2, "total": D('15')},
-            {'day_in': td, 'day_out': nd, "guests": 3, "total": None},
+            {'day_in': d1, 'day_out': d2, "guests": 1, "total": D('10')},
+            {'day_in': d1, 'day_out': d2, "guests": 2, "total": D('15')},
+            {'day_in': d1, 'day_out': d2, "guests": 3, "total": None},
             # two days
-            {'day_in': td, 'day_out': nd+od, "guests": 1, "total": D('21')},
-            {'day_in': td, 'day_out': nd+od, "guests": 2, "total": D('27')},
-            {'day_in': td, 'day_out': nd+od, "guests": 3, "total": None},
+            {'day_in': d1, 'day_out': d3, "guests": 1, "total": D('21')},
+            {'day_in': d1, 'day_out': d3, "guests": 2, "total": D('27')},
+            {'day_in': d1, 'day_out': d3, "guests": 3, "total": None},
             # three days
-            {'day_in': td, 'day_out': nd+od*2, "guests": 1, "total": D('35')},
-            {'day_in': td, 'day_out': nd+od*2, "guests": 2, "total": D('42')},
-            {'day_in': td, 'day_out': nd+od*2, "guests": 3, "total": None},
+            {'day_in': d1, 'day_out': d4, "guests": 1, "total": D('35')},
+            {'day_in': d1, 'day_out': d4, "guests": 2, "total": D('42')},
+            {'day_in': d1, 'day_out': d4, "guests": 3, "total": None},
         ]
-        for p in prices:
+        for cnt, p in enumerate(prices):
             price = self.room.get_price(date_in=p['day_in'],
                 date_out=p['day_out'], guests=p['guests'])
             self.assertEqual(price, p['total'])
